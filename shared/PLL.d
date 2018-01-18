60bdfd764e61ad8a3f43f526bee7d82061262455
Josh Minor <jishminor@gmail.com>
2018-01-18 13:21:09 -0600

    clean up

--- shared/PLL.d
+++
@@ -1,9 +0,0 @@
-../shared/PLL.o: ../shared/PLL.c ../shared/PLL.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/stdint.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/machine/_default_types.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/sys/features.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/_newlib_version.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/sys/_intsup.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/sys/_stdint.h \
- ../TivaWare/inc/tm4c123gh6pm.h
--- shared/startup.d
+++
@@ -1,9 +0,0 @@
-../shared/startup.o: ../shared/startup.c \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/stdint.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/machine/_default_types.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/sys/features.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/_newlib_version.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/sys/_intsup.h \
- /usr/local/Cellar/arm-gcc-bin/6-2017-q2-update/arm-none-eabi/include/sys/_stdint.h \
- ../TivaWare/inc/hw_nvic.h ../TivaWare/inc/hw_types.h
